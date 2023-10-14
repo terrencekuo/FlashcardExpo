d1b342fdc41e0e2e8d3cc731479e7b4b549f5959
terrencekuo <kuuott@gmail.com>
2023-10-15 00:18:57 +0800

    fix navigation to study decks

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import { Button } from 'react-native';
 import HomeScreen from '../screens/HomeScreen';
 import CreateDeckScreen from '../screens/CreateDeckScreen';
-import DeckBrowserScreen from '../screens/DeckBrowserScreen';
 import CreateFlashcardScreen from '../screens/CreateFlashcardScreen';
+import DeckBrowserScreen from '../screens/DeckBrowserScreen';
 import StudyScreen from '../screens/StudyScreen';
 
 const Stack = createStackNavigator();
@@ -13,8 +14,19 @@ function AppNavigator() {
     <Stack.Navigator initialRouteName="Home">
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="CreateDeck" component={CreateDeckScreen} />
-      <Stack.Screen name="DeckBrowser" component={DeckBrowserScreen} />
       <Stack.Screen name="CreateFlashcard" component={CreateFlashcardScreen} />
+      <Stack.Screen 
+        name="DeckBrowser" 
+        component={DeckBrowserScreen}
+        options={({ navigation }) => ({
+          headerLeft: () => (
+            <Button 
+              onPress={() => navigation.navigate('Home')} 
+              title="Home" 
+            />
+          )
+        })}
+      />
       <Stack.Screen name="Study" component={StudyScreen} />
     </Stack.Navigator>
   );
