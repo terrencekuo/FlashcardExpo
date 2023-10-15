54c3c80249210cb884c6b8b74a8e9af95b09893b
terrencekuo <kuuott@gmail.com>
2023-10-15 22:00:26 +0800

    add persistent storage

--- App.js
+++ App.js
@@ -3,13 +3,38 @@ import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './navigation/AppNavigator';
 import { Provider } from 'react-redux';
 import store from './store';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+function AppInitializer({ children }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const savedDecks = await AsyncStorage.getItem('decks');
+        if (savedDecks) {
+          dispatch({ type: 'INITIALIZE_DECKS', decks: JSON.parse(savedDecks) });
+        }
+      } catch (error) {
+        console.error("Error fetching decks from AsyncStorage:", error);
+      }
+    }
+    fetchData();
+  }, []);
+
+  return children;
+}
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <AppNavigator />
-      </NavigationContainer>
+      <AppInitializer>
+        <NavigationContainer>
+          <AppNavigator />
+        </NavigationContainer>
+      </AppInitializer>
     </Provider>
   );
 }
--- reducers/index.js
+++ reducers/index.js
@@ -1,23 +1,30 @@
 import { ADD_DECK, ADD_CARD } from '../actions/index';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 function decks(state = {}, action) {
   switch (action.type) {
+    case 'INITIALIZE_DECKS':
+      return action.decks;
     case ADD_DECK:
-      return {
+      const newStateDeck = {
         ...state,
         [action.title]: {
           title: action.title,
           questions: [],
         },
       };
+      AsyncStorage.setItem('decks', JSON.stringify(newStateDeck));
+      return newStateDeck;
     case ADD_CARD:
-      return {
+      const newStateCard = {
         ...state,
         [action.title]: {
           ...state[action.title],
           questions: state[action.title].questions.concat([action.card]),
         },
       };
+      AsyncStorage.setItem('decks', JSON.stringify(newStateCard));
+      return newStateCard;
     default:
       return state;
   }
