c61b36e928ca819f02e01d8b2fdac1d7dd057c67
terrencekuo <kuuott@gmail.com>
2023-10-21 17:28:15 +0800

    more flashcard cleanup

--- screens/CreateFlashcardScreen.js
+++ screens/CreateFlashcardScreen.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
+import React, { useState, useRef } from 'react';  // Added useRef
+import { View, Text, TextInput, Button, StyleSheet, Keyboard } from 'react-native';  // Added Keyboard
 import { useDispatch } from 'react-redux';
 import { addCardToDeck } from '../actions';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
@@ -15,6 +15,9 @@ export default function CreateFlashcardScreen({ route, navigation }) {
         { label: 'Pinyin', value: '' }
     ];
     const [sides, setSides] = useState(defaultSides);
+
+    // Create refs for the TextInput components
+    const inputRefs = useRef(sides.map(() => React.createRef()));
 
     // Handle changing the text of a side
     const handleSideChange = (text, index) => {
@@ -35,32 +38,41 @@ export default function CreateFlashcardScreen({ route, navigation }) {
     };
 
     // Navigation function to return to the home screen
-    const done = () => {
+    const handleStartStudying = () => {
         navigation.navigate('Home');
     };
 
     return (
-      <KeyboardAwareScrollView style={styles.container}>
-          {/* Render each side input */}
-          {sides.map((side, index) => (
-              <View key={index} style={styles.sideContainer}>
-                  <Text>{side.label}</Text>
-                  <TextInput
-                      style={styles.input}
-                      value={side.value}
-                      onChangeText={(text) => handleSideChange(text, index)}
-                      placeholder={`Enter ${side.label}`}
-                  />
-              </View>
-          ))}
-
-          {/* Footer buttons */}
-          <View style={styles.bottomSection}>
-              <Button title="Done" onPress={done} />
-              <Button title="Add Flash Card" onPress={handleAddCard} />
-          </View>
-      </KeyboardAwareScrollView>
-  );
+        <KeyboardAwareScrollView style={styles.container}>
+            {/* Render each side input */}
+            {sides.map((side, index) => (
+                <View key={index} style={styles.sideContainer}>
+                    <Text>{side.label}</Text>
+                    <TextInput
+                        ref={inputRefs.current[index]}
+                        style={styles.input}
+                        value={side.value}
+                        onChangeText={(text) => handleSideChange(text, index)}
+                        placeholder={`Enter ${side.label}`}
+                        onSubmitEditing={() => {
+                            if (index < sides.length - 1) {
+                                inputRefs.current[index + 1].current.focus();
+                            } else {
+                                Keyboard.dismiss();
+                            }
+                        }}
+                        returnKeyType={index < sides.length - 1 ? "next" : "done"}
+                    />
+                </View>
+            ))}
+  
+            {/* Footer buttons */}
+            <View style={styles.bottomSection}>
+                <Button title="Start Studying" onPress={handleStartStudying} />
+                <Button title="Add Flash Card" onPress={handleAddCard} />
+            </View>
+        </KeyboardAwareScrollView>
+    );
 }
 
 const styles = StyleSheet.create({
@@ -71,17 +83,19 @@ const styles = StyleSheet.create({
     sideContainer: {
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent: 'space-between',  // Pushes children to the edges
         marginBottom: 15,
     },
     input: {
-        flex: 1,
+        flex: 1,  // Takes up all available space
         padding: 10,
         borderWidth: 1,
         borderColor: 'grey',
         borderRadius: 5,
+        marginLeft: 10,  // Adds some spacing between the Text and TextInput
     },
     bottomSection: {
-        marginTop: 'auto',  // this pushes the footer to the bottom
+        marginTop: 'auto',
         flexDirection: 'row',
         justifyContent: 'space-between',
     },
