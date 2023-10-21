80b43fa414ef7385cbb5d9810dc74aa81a4d7318
terrencekuo <kuuott@gmail.com>
2023-10-21 17:16:23 +0800

    cleanup create flash cards

--- screens/CreateFlashcardScreen.js
+++ screens/CreateFlashcardScreen.js
@@ -2,15 +2,13 @@ import React, { useState } from 'react';
 import { View, Text, TextInput, TouchableOpacity, Button, StyleSheet } from 'react-native';
 import { useDispatch } from 'react-redux';
 import { addCardToDeck } from '../actions';
-import { Ionicons } from '@expo/vector-icons';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
-
-const MAX_SIDES = 3;
 
 export default function CreateFlashcardScreen({ route, navigation }) {
     const { deckName } = route.params;
     const dispatch = useDispatch();
 
+    // Default sides for the flashcard
     const defaultSides = [
         { label: 'Native Language', value: '' },
         { label: 'Chinese Characters', value: '' },
@@ -18,25 +16,14 @@ export default function CreateFlashcardScreen({ route, navigation }) {
     ];
     const [sides, setSides] = useState(defaultSides);
 
-    // Function to handle addition of a new side
-    const handleAddSide = () => {
-        if (sides.length < MAX_SIDES) {
-            setSides(prevSides => [...prevSides, '']);
-        }
-    };
-
-    // Function to remove a side based on its index
-    const handleRemoveSide = (indexToRemove) => {
-        setSides(prevSides => prevSides.filter((_, index) => index !== indexToRemove));
-    };
-
-    // Function to handle changing the text of a side
+    // Handle changing the text of a side
     const handleSideChange = (text, index) => {
         const newSides = [...sides];
         newSides[index].value = text;
         setSides(newSides);
     };
 
+    // Handle adding the card to the deck
     const handleAddCard = () => {
         if (sides.some(side => side.value.trim() === '')) {
             alert('Please fill out all sides!');
@@ -48,45 +35,30 @@ export default function CreateFlashcardScreen({ route, navigation }) {
     };
 
     // Navigation function to return to the home screen
-    const handleStartStudying = () => {
+    const done = () => {
         navigation.navigate('Home');
     };
 
     return (
       <KeyboardAwareScrollView style={styles.container}>
-
-          {/* Sides Input Rows */}
-          <View style={styles.rowSection}>
-            {sides.map((side, index) => (
-                <View key={index} style={styles.sideContainer}>
-                    <Text>{side.label}</Text>
-                    <TextInput
-                        style={[
-                            styles.input,
-                            (index === sides.length - 1 && index >= 2) && styles.inputWithTab
-                        ]}
-                        value={side.value}
-                        onChangeText={(text) => handleSideChange(text, index)}
-                        placeholder={`Enter ${side.label}`}
-                    />
-                    {index === sides.length - 1 && index >= 2 && (
-                        <TouchableOpacity onPress={() => handleRemoveSide(index)} style={styles.deleteTab} />
-                    )}
-                </View>
-            ))}
-            {sides.length < MAX_SIDES && (
-                <TouchableOpacity onPress={handleAddSide} style={styles.addButtonContainer}>
-                    <Ionicons name="add-circle-outline" size={24} color="blue" />
-                </TouchableOpacity>
-            )}
-          </View>
+          {/* Render each side input */}
+          {sides.map((side, index) => (
+              <View key={index} style={styles.sideContainer}>
+                  <Text>{side.label}</Text>
+                  <TextInput
+                      style={styles.input}
+                      value={side.value}
+                      onChangeText={(text) => handleSideChange(text, index)}
+                      placeholder={`Enter ${side.label}`}
+                  />
+              </View>
+          ))}
 
           {/* Footer buttons */}
           <View style={styles.bottomSection}>
-              <Button title="Start Studying" onPress={handleStartStudying} />
+              <Button title="Done" onPress={done} />
               <Button title="Add Flash Card" onPress={handleAddCard} />
           </View>
-
       </KeyboardAwareScrollView>
   );
 }
@@ -95,9 +67,6 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         padding: 20,
-    },
-    rowSection: {
-        marginBottom: 15,  // gives some space between rows and card
     },
     sideContainer: {
         flexDirection: 'row',
@@ -110,23 +79,6 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: 'grey',
         borderRadius: 5,
-    },
-    inputWithTab: {
-        marginRight: 8,
-    },
-    deleteTab: {
-        width: 8,
-        height: '100%',
-        backgroundColor: 'red',
-        borderTopRightRadius: 5,
-        borderBottomRightRadius: 5,
-        position: 'absolute',
-        right: 0,
-        top: 0,
-    },
-    addButtonContainer: {
-        alignItems: 'flex-end',
-        marginBottom: 15,
     },
     bottomSection: {
         marginTop: 'auto',  // this pushes the footer to the bottom
