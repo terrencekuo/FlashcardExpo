31cf117024692f6168013540f79897e5825e5bf9
terrencekuo <kuuott@gmail.com>
2023-12-25 09:40:17 -0800

    beautify the topic create + move

--- screens/TopicSelectionScreen.js
+++ screens/TopicSelectionScreen.js
@@ -1,12 +1,11 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, FlatList, StyleSheet, TextInput, Button } from 'react-native';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 function TopicSelectionScreen({ route, navigation }) {
   const { selectedDecks, onTopicSelect } = route.params;
   const topics = useSelector(state => state.topics);
   const [newTopicName, setNewTopicName] = useState('');
-  const dispatch = useDispatch();
 
   const handleTopicSelect = (topicName, isNewTopic = false) => {
     onTopicSelect(topicName, isNewTopic);
@@ -23,23 +22,32 @@ function TopicSelectionScreen({ route, navigation }) {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={Object.keys(topics)}
-        keyExtractor={(item) => item}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.topicItem} onPress={() => handleTopicSelect(item)}>
-            <Text style={styles.topicText}>{item}</Text>
-          </TouchableOpacity>
-        )}
-      />
-      <View style={styles.newTopicContainer}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter new topic name"
-          value={newTopicName}
-          onChangeText={setNewTopicName}
+      {/* Existing Topics Section */}
+      <View style={styles.topicsBox}>
+        <Text style={styles.boxHeader}>Move to Existing Topic</Text>
+        <FlatList
+          data={Object.keys(topics)}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <TouchableOpacity style={styles.topicItem} onPress={() => handleTopicSelect(item)}>
+              <Text style={styles.topicText}>{item}</Text>
+            </TouchableOpacity>
+          )}
         />
-        <Button title="Create New Topic" onPress={createNewTopic} />
+      </View>
+      
+      {/* New Topic Creation Section */}
+      <View style={styles.newTopicBox}>
+        <Text style={styles.boxHeader}>Create New Topic</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={styles.input}
+            placeholder="Enter new topic name"
+            value={newTopicName}
+            onChangeText={setNewTopicName}
+          />
+          <Button title="Create" onPress={createNewTopic} />
+        </View>
       </View>
     </View>
   );
@@ -49,6 +57,29 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'center',
+  },
+  topicsBox: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    marginBottom: 20,
+  },
+  newTopicBox: {
+    marginHorizontal: 20,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: '#ddd',
+  },
+  boxHeader: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    padding: 15,
+    backgroundColor: '#f0f0f0',
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    textAlign: 'center',
   },
   topicItem: {
     padding: 15,
@@ -56,17 +87,19 @@ const styles = StyleSheet.create({
     borderBottomColor: '#ddd',
   },
   topicText: {
-    fontSize: 18,
+    fontSize: 16,
   },
-  newTopicContainer: {
-    paddingHorizontal: 20,
-    paddingTop: 20,
+  inputContainer: {
+    marginTop: 10,
+    padding: 15,
   },
   input: {
     height: 40,
     borderColor: 'gray',
     borderWidth: 1,
     marginBottom: 10,
+    borderRadius: 5,
+    paddingHorizontal: 10,
   },
 });
 
